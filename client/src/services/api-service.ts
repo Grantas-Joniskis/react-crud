ee2cf6f1b6d582dfe3bfd3ba233d7368b178e2a7
Grantas-Joniskis <grantasjo@gmail.com>
2023-02-20 17:23:34 +0200

    feat: creating api-service to fetch data

---
+++ client/src/services/api-service.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:5024/blogs',
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+});
+
+const fetchBlogs = async () => {
+  const response = await axios.get('/blogs');
+  return response.data;
+};
