1d800b30737465d450231e0df1e91cc8e3fe2978
Grantas-Joniskis <grantasjo@gmail.com>
2023-02-20 17:40:01 +0200

    feat: displaying data in the home-page

--- client/src/pages/home-page.tsx
+++ client/src/pages/home-page.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import { Container } from '@mui/material';
+import { Box, Container } from '@mui/material';
+import ApiService from 'services/api-service';
 
-const HomePage = () => (
-  <Container sx={{ my: 6 }} />
-);
+const HomePage = () => {
+  const [blogs, setBlogs] = React.useState<BlogModel[]>([]);
 
+  React.useEffect(() => {
+    (async () => {
+      const fetchedBlogs = await ApiService.fetchBlogs();
+      setBlogs(fetchedBlogs);
+    })();
+  }, []);
+
+  return (
+    <Container>
+      <Box component="pre">
+        {JSON.stringify(blogs, null, 4)}
+      </Box>
+    </Container>
+  );
+};
 export default HomePage;
